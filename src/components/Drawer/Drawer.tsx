402c8ad3b910f04d20e6c8171c2287d1b2d5266b
Shrikant Shet <sshet24@gmail.com>
2023-02-14 21:50:23 +0530

    Use portal for drawer

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { createPortal } from "react-dom";
 
 interface Props {
   isVisible: boolean;
@@ -16,30 +17,41 @@ function Drawer(props: Props) {
     sideContent,
     end = false,
   } = props;
-  return (
-    <div
-      className={`
-        drawer fixed top-0 left-0 w-full h-full
-        ${end ? "drawer-end" : ""} 
-    `}
-    >
-      <input
-        type="checkbox"
-        className="drawer-toggle"
-        checked={isVisible}
-        readOnly
-      />
-      <div className="drawer-content">{children}</div>
-      <div className="drawer-side">
+
+  const ref = useRef<Element | null>(null);
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    ref.current = document.querySelector<HTMLElement>("body");
+    setMounted(true);
+  }, []);
+
+  return mounted && ref.current
+    ? createPortal(
         <div
-          role="presentation"
-          className="drawer-overlay"
-          onClick={onOverlayClick}
-        />
-        <div className="w-80 bg-base-100">{sideContent}</div>
-      </div>
-    </div>
-  );
+          className={`drawer fixed top-0 left-0 w-full h-full ${
+            end ? "drawer-end" : ""
+          }`}
+        >
+          <input
+            type="checkbox"
+            className="drawer-toggle"
+            checked={isVisible}
+            readOnly
+          />
+          <div className="drawer-content">{children}</div>
+          <div className="drawer-side">
+            <div
+              role="presentation"
+              className="drawer-overlay"
+              onClick={onOverlayClick}
+            />
+            <div className="w-80 bg-base-100">{sideContent}</div>
+          </div>
+        </div>,
+        document.body
+      )
+    : null;
 }
 
 export default Drawer;
